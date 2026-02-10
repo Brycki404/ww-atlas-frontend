38bc8296567a066a06849bbab5c4c82fbf47db7e
B0NBunny <dustinbk@icloud.com>
2026-02-10 18:20:58 -0500

    agggg

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from "path";
+import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ww-atlas-frontend/",
+  base: '/ww-atlas-frontend/',
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "index.html"),
-        404: resolve(__dirname, "index.html"),
+        main: resolve(__dirname, 'index.html'),
+        404: resolve(__dirname, 'index.html'),
       },
     },
   },
-});
+})
