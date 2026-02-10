a02bb3dfd2f66c08668df4aa5c36562ac4e045cd
B0NBunny <dustinbk@icloud.com>
2026-02-10 17:11:41 -0500

    upd

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import './index.css'
-import App from './App.tsx'
-
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <BrowserRouter basename="/ww-atlas-frontend">
-      <App />
-    </BrowserRouter>
-  </StrictMode>
-);
+import "./index.css";
+import App from "./App";
 
 // Create or load user ID
 let userId = localStorage.getItem("user_id");
@@ -22,8 +14,10 @@ if (!userId) {
 export const USER_ID = userId;
 export const API_URL = "https://ww-atlas.up.railway.app";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter basename="/ww-atlas-frontend">
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
