4c82932224598f5b31e8e39fa810f1038a672ec7
B0NBunny <dustinbk@icloud.com>
2026-02-10 17:08:13 -0500

    oh my gosh I wasn't even routing the discord callback

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Routes, Route } from "react-router-dom";
 import MapPage from "./components/MapPage";
+import DiscordCallback from "./components/DiscordCallback";
 
 export default function App() {
-  return <MapPage />;
+  return (
+    <Routes>
+      <Route path="/" element={<MapPage />} />
+      <Route path="/discord/callback" element={<DiscordCallback />} />
+    </Routes>
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from "react-router-dom";
 import './index.css'
 import App from './App.tsx'
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <BrowserRouter basename="/ww-atlas-frontend">
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
 
 // Create or load user ID
 let userId = localStorage.getItem("user_id");
